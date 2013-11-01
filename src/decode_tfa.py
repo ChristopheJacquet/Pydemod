80ac7d193290be04379170fe6e61490354a03794
Christophe Jacquet <ChristopheJacquet@users.noreply.github.com>
2013-11-01 22:38:35 +0000

    Adaptation to variations of the protocol
    git-svn-id: https://pydemod.googlecode.com/svn/trunk@5 5e9d3692-3211-ec77-f7b1-2e1477366885

--- src/decode_tfa.py
+++ src/decode_tfa.py
@@ -2,7 +2,7 @@
 
 #
 # Frame structure on an example:
-#   AA - sync byte
+#   AA - sync byte (sometimes multiple sync bytes)
 #   2D \ presumably a vendor/sensor type identifier
 #   D4 /
 #   96 \   follows the same protocol as LaCrosse's TX29  -> 9 nibbles following
@@ -21,22 +21,40 @@ import numpy
 
 import sys
 
-(sampleRate, samples) = wavfile.read(sys.argv[1])
 
-frame = logic.decode_0xAA_prefixed_frame(samples, sampleRate)
+import argparse
 
-if frame.size < 64:
-    frame = numpy.append(frame, [0] * (64 - frame.size))
+parser = argparse.ArgumentParser(description='Decodes TFA temperature and humidity sensors')
 
-print "Frame: size {0} bits, contents {1}".format(frame.size, frame)
+parser.add_argument("filename", type=str, help='WAV file to process')
+parser.add_argument("--bitrate", type=int, default=17200, help='Bit rate of the transmission, in bit/s')
+parser.add_argument("--synclen", type=int, default=8, help='Number of sync bits (these bits will be ignored)')
+parser.add_argument("--verbose", help='Print additional debug messages', action="store_true")
 
-if frame.size >= 64:
-    frame = frame[:64]
-    matrix = numpy.mat(numpy.reshape(frame, (8, 8)))
+args = parser.parse_args()
+
+bitrate = args.bitrate
+synclen = args.synclen
+framelen = 56 + synclen
+
+print("Bitrate: {0}, synclen: {1}, framelen: {2}".format(bitrate, synclen, framelen))
+
+(sampleRate, samples) = wavfile.read(args.filename)
+
+frame = logic.decode_0xAA_prefixed_frame(samples, sampleRate, bitrate=bitrate, verbose=args.verbose)
+
+if frame.size < framelen:
+    frame = numpy.append(frame, [0] * (framelen - frame.size))
+
+print "Frame: size {0} bits, contents {1}, framelen={2}".format(frame.size, frame, framelen)
+
+if frame.size >= framelen:
+    frame = frame[:framelen]
+    # reconstruct bytes
+    matrix = numpy.mat(numpy.reshape(frame, (framelen/8, 8)))
     byteSeq = matrix * numpy.mat("128;64;32;16;8;4;2;1")
-    allBytes = [int(byteSeq[i]) for i in range(0,8)]
-    messageBytes = allBytes[0:7]
-    print "Frame hex contents: " + " ".join(["{0:02X}".format(b) for b in messageBytes])
+    allBytes = [int(byteSeq[i]) for i in range(0,len(byteSeq))]
+    print "Frame hex contents: " + " ".join(["{0:02X}".format(b) for b in allBytes])
      
-    lacrosse.decode_tx29(frame[24:])
+    lacrosse.decode_tx29(frame[(synclen + 16):])
     
--- src/pydemod/coding/logic.py
+++ src/pydemod/coding/logic.py
@@ -1,21 +1,22 @@
 import numpy
 
-def decode_0xAA_prefixed_frame(samples, sampleRate):
-    threshold = ( max(samples) + min(samples) ) / 2
+def decode_0xAA_prefixed_frame(samples, sampleRate, bitrate=17258, verbose=False):
+    threshold = numpy.mean(samples)
 
-    print("Min={0}, Max={1}, Threshold={2}".format(min(samples), max(samples), threshold))
+    if verbose:
+        print("Min={0}, Max={1}, Threshold={2}".format(min(samples), max(samples), threshold))
     
     signs = numpy.array(samples > threshold, int)
     differences = numpy.diff(signs)
     changes = numpy.nonzero(differences)[0]
 
-    print("27:{2}, 28:{0}, 29:{1}".format(signs[28], signs[29], signs[27]))
+    if verbose:    
+        print "{0} edges at instants: {1}".format(changes.size, changes)
     
-    print "{0} edges at instants: {1}".format(changes.size, changes)
+    bitlen = sampleRate / bitrate
     
-    bitlen = 356./64. * sampleRate/ 96000     # measured duration at 96kHz: 356 samples (17258 bit/s)
-    
-    print "Theoretical bit length: {0} samples".format(bitlen)
+    if verbose:
+        print "Theoretical bit length: {0} samples".format(bitlen)
     
     result = numpy.array([])
     
@@ -28,21 +29,25 @@ def decode_0xAA_prefixed_frame(samples, sampleRate):
         bit = signs[i]
 
         if precBit != bit:
-            print("Transition at {0}, expected at {1}".format(i, bitlen/2-bitpos+i))
+            if verbose:
+                print("Transition at {0}, expected at {1}".format(i, bitlen/2-bitpos+i))
         
             if bitpos < bitlen/2-1:
-                bitpos += bitlen/20.
-                print("(+)")
+                bitpos += bitlen/10.
+                if verbose:
+                    print("(+)")
             elif bitpos > bitlen/2+1:
-                bitpos -= bitlen/20.
-                print("(-)")
+                bitpos -= bitlen/10.
+                if verbose:
+                    print("(-)")
 
         
         if bitpos >= bitlen:
             result = numpy.append(result, bit)
             bitpos -= bitlen
             
-            print("Sample at {0} => {1} (bitpos={2})".format(i, bit, bitpos))
+            if verbose:
+                print("Sample at {0} => {1} (bitpos={2})".format(i, bit, bitpos))
 
     
         precBit = bit
